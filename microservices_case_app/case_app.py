2e290902228e55d8f40fdbeb86175cabbaac2355
SHARP <ssharp9999@gmail.com>
2023-02-12 17:14:44 -0800

    example request

--- microservices_case_app/case_app.py
+++ microservices_case_app/case_app.py
@@ -3,11 +3,12 @@ import time
 def request_chart():
     """
     Method to request a chart with data based on data from case app in the case_database
-    Just to show how requests can be made the informaiton that is received back
+    This is to show how requests can be made the data that is received back
     """
-    # open the file and write to it the provided instruction (file is properly closed)
-    with open('chart_service.txt', "w") as file:
-        file.write('case_database.csv')
+
+    with open('chart_service.txt', "w") as f:
+        lines = ["createChart\n", "case_database.csv\n"]
+        f.writelines(lines)
 
     # timer to show that the service file is updated with
     time.sleep(45)
